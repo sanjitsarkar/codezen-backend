80d4686368732bc98b70547833ed80b99d0aad0f
Sanjit Sarkar <ssanjit856@gmail.com>
2021-09-01 13:07:53 +0530

    added meta tag

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -9,7 +9,7 @@ const LandingPage = () => {
              <Signup isModalOpen={isModalOpenSignup} closeModal={()=>setIsModalOpenSignup(false)}/>
             <div className="top">
                 <h2>Share and run your Code in realtime</h2>
-                <p>An online editor competitive coder, interviews, teaching and many more...</p>
+                <p>An online editor for competitive coder, interviews, teaching and many more...</p>
                 <button onClick={(e) => { setIsModalOpenSignup(!isModalOpenSignup) }}>Register Now</button>
             </div>
             {/* <img srcSet={CodeIMG}/> */}
